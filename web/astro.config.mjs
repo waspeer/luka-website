eadf5d1a92054a0d340c161f780c1b753cfc0b26
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2022-07-07 22:02:43 +0200

    feat: add robots.txt

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,14 +1,18 @@
 import sitemap from '@astrojs/sitemap';
 import solid from '@astrojs/solid-js';
+import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), sitemap()],
+  integrations: [solid(), sitemap(), robotsTxt()],
   site: 'https://lisaluka.com',
   vite: {
     ssr: {
       external: ['svgo'],
     },
+  },
+  experimental: {
+    integrations: true,
   },
 });
