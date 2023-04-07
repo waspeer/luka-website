4151b89ce0f50368dd7ba681522ca3d795ed8a49
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2023-04-07 10:24:16 +0200

    upgrade deps

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -5,8 +5,6 @@ import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
