5d4faf81158a91accb642b56ca65ff203b0d1c24
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2024-06-06 12:42:02 +0200

    enable ssr

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,10 +1,12 @@
-import solidJs from "@astrojs/solid-js";
+import netlify from '@astrojs/netlify';
+import solidJs from '@astrojs/solid-js';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
-
-import tailwind from "@astrojs/tailwind";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), solidJs(), tailwind()]
+  integrations: [icon(), solidJs(), tailwind()],
+  output: 'server',
+  adapter: netlify(),
 });
