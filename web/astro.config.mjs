6d4a40f6e96cdec521b8e1ca426e804767eaac59
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2024-06-06 12:55:21 +0200

    halp

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -7,6 +7,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({ edgeMiddleware: true }),
   integrations: [icon(), solidJs(), tailwind()],
 });
