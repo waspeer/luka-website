199e2efff89e527175609db14705cab51e8d81dc
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2024-06-06 12:50:47 +0200

    vercel, help?

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,12 +1,12 @@
-import netlify from '@astrojs/netlify';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: vercel(),
   integrations: [icon(), solidJs(), tailwind()],
 });
